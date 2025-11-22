132f13048d737b905642978a6fcdc74c402a2a38
Joe <JoeBloggs035@gmail.com>
2025-11-22 23:58:03 +0300

    assert элемента на странице и перехват исключения NoSuchElementException

--- test_main_page.py
+++ test_main_page.py
@@ -10,14 +10,16 @@ def test_guest_can_go_to_login_page(browser):
         browser, link
     )  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
-    #time.sleep(10)
+    # time.sleep(10)
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логина
+
 
 def test_guest_should_see_login_link(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
+
 
 """
 from selenium.webdriver.common.by import By
